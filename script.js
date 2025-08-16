aa2f9b19732b8fdffff81a42ddd042452819a30e
Carmin Dias Ilarraz <144161912+Carmin365@users.noreply.github.com>
2025-08-16 01:43:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function adicionarAoCarrinho(nomeDoProduto) {
+    alert('Produto "' + nomeDoProduto + '" adicionado ao seu carrinho!');
+    console.log('Carrinho atualizado com: ' + nomeDoProduto);
+}
